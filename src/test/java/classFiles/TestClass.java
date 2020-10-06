ca9d2aaf61bfa49e2cf1a76fa96d655559dd7ac8
munee <munee@LAPTOP-K565CJIQ>
2020-10-06 22:59:34 +0530

    committing TestClass.java to BranchA

--- src/test/java/classFiles/TestClass.java
+++ src/test/java/classFiles/TestClass.java
@@ -1,13 +1,46 @@
+/* THIS IS A PRACTICE WORK ON GIT AS A BEGINNER */
+
 package classFiles;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestClass {
 	
+	static WebDriver driver;
 	
+  @BeforeTest
+  public static void browserSetUp() 
+   {
+	 System.setProperty("webdriver.chrome.driver","C:\\Users\\munee\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	 driver = new ChromeDriver();
+   }
+  
+  // Open a web page
+  
   @Test(priority=0)
-  public void f() {
+  public static void launchGoogle() throws InterruptedException
+  {
+	  driver.get("https://www.google.com/");
+	  Thread.sleep(3000);
 	  
-	  System.out.println("Add to index check going on");
+	  System.out.println("This is a practice project in Git");
+	  driver.close();	  
   }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
